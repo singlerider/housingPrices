2d99a305fb454730e4818dbfe8172ce063c920b9
Shane Engelman <haroboy876@gmail.com>
2015-08-28 15:36:20 -0700

    add housing_prices.py

---
+++ housing_prices.py
@@ -0,0 +1,20 @@
+import requests
+import csv
+
+app = Blueprint('housing_prices', __name__)
+
+zip = "94109"
+
+def get_estimated_rent_prices(zip):
+    url = "https://www.quandl.com/api/v1/datasets/ZILL/Z{}_RAH.csv".format(zip)
+    response = requests.get(url=url)
+    prices = []
+    for entry in response.content.split("\n"):
+        prices.append(entry.split(','))
+    prices_dict = {}
+    for i in prices[1:len(prices)-1]:
+        prices_dict[i[0]] = float(i[1])
+    return prices_dict
+    
+if __name__ == "__main__":
+    return get_estimated_rent_prices(zip)
