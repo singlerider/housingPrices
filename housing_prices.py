b7864a1ddeef41063cf44d3cedc6a1d85ec0067d
Shane <haroboy876@gmail.com>
2015-08-28 22:38:05 -0700

    add all data types to search from

--- housing_prices.py
+++ housing_prices.py
@@ -1,10 +1,48 @@
+#!/usr/bin/env python
 import requests
 import csv
 
-zip = "94109"
+zipcode = "94109"
+code = "PRR"
 
-def get_estimated_rent_prices(zip):
-    url = "https://www.quandl.com/api/v1/datasets/ZILL/Z{}_RAH.csv".format(zip)
+# Below are the codes for all types of readable information
+
+data_types = {
+"A": "All Homes",
+"SF": "Single Family Residences",
+"C": "Condominiums",
+"MVSF": "Price per Square Foot",
+"1B": "1 Bedroom",
+"2B": "2 Bedroom",
+"3B": "3 Bedroom",
+"4B": "4 Bedroom",
+"5B": "5 Bedroom",
+"BT": "Bottom Tier",
+"MT": "Middle Tier",
+"TT": "Top Tier",
+"RMP": "Median Rent, Homes Listed for Rent",
+"RAH": "Estimated Rent, All Homes in Region",
+"RZSF": "Estimated Rent per Square Foot",
+"PRR": "Price-to-Rent Ratio",
+"MLP": "Median List Price",
+"MSP": "Median Sale Price",
+"MLPSF": "Median List Price per Square Foot",
+"MSPSF": "Median Sale Price per Square Foot",
+"LPC": "Listing with Price Cut in Lastt 30 Days",
+"MPC": "Median Price Cut",
+"SLPR": "Ratio of Sale Price to List Price",
+"SFL": "Sold for Loss",
+"SFG": "Sold for Gain",
+"IV": "Increasing Values",
+"DV": "Decreasing Values",
+"SPY": "Turnover in Housing Market, Past 1 Year",
+"HR": "Number of Homes for Rent",
+"HF": "Monthly Foreclosures per 10,000 Homes",
+"FR": "Percentage of Sales / Foreclosures"
+             }
+
+def get_estimated_rent_prices(zipcode, code):
+    url = "https://www.quandl.com/api/v1/datasets/ZILL/Z{}_{}.csv".format(zipcode, code)
     response = requests.get(url=url)
     prices = []
     for entry in response.content.split("\n"):
@@ -12,7 +50,8 @@ def get_estimated_rent_prices(zip):
     prices_dict = {}
     for i in prices[1:len(prices)-1]:
         prices_dict[i[0]] = float(i[1])
-    return prices_dict
+    aggregated_data = {"description": data_types[code], code : prices_dict}
+    return aggregated_data
     
 if __name__ == "__main__":
-    return get_estimated_rent_prices(zip)
+   print  get_estimated_rent_prices(zipcode, code)
